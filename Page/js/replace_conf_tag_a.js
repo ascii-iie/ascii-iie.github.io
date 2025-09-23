5c5c1de1dc100e72e23784d0626faf2899c9c951
ascii-iie <96916359+ascii-iie@users.noreply.github.com>
2025-09-23 13:05:15 +0800

    NeurIPS2025更新

--- Page/js/replace_conf_tag_a.js
+++ Page/js/replace_conf_tag_a.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
     {
       level: "CCF-B",
       colorClass: "text-bg-warning",
-      confs: ["CIKM2020", "COLING2024", "COLING2023", "COLING2022", "ECAI2025", "ECAI2023", "ECML2025", "ECML2021","EMNLP2025",
+      confs: ["CIKM2020", "COLING2024", "COLING2023", "COLING2022", "ECAI2025", "ECAI2023", "ECML2025", "ECML2021", "EMNLP2025",
               "EMNLP2023", "EMNLP2022", "EMNLP2021", "EMNLP2020", "ICASSP2024", "ICASSP2022", "ICASSP2021",
               "ICDM2024", "ICDM2022", "ICDM2021", "ICDM2020", "ICDM2019", "ICME2019", "NAACL2024", "PR2022", "TNNLS2022", "WSDM2024", "WSDM2021",
               "WWWJ2024", "WWWJ2023", "WWWJ2022", "WWWJ2020"]
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
     {
       level: "CCF-A",
       colorClass: "text-bg-danger",
-      confs: ["AAAI2025", "AAAI2023", "AAAI2022", "AAAI2021", "AAAI2020",
+      confs: ["NeurIPS2025", "AAAI2025", "AAAI2023", "AAAI2022", "AAAI2021", "AAAI2020",
               "ACL2025", "ACL2024", "ACL2023", "ACL2022", "ACL2021",
               "ICML2024", "KDD2025", "SIGIR2024", "SIGIR2022", "TKDE2023", "TOIS2025", "TOIS2023",
               "WWW2025", "WWW2024", "WWW2023", "WWW2022", "WWW2021", "WWW2020", "WWW2019"]
@@ -38,7 +38,7 @@ document.addEventListener("DOMContentLoaded", function () {
       cfg.confs.forEach(conf => {
         const regex = new RegExp(`${conf}:`, "g");
         html = html.replace(regex, 
-          `<span class="badge text-bg-success">${conf}</span> <span class="badge ${cfg.colorClass} rounded-pill">${cfg.level}</span>`);
+          `<span class="badge ${cfg.colorClass}">${conf}</span> <span class="badge ${cfg.colorClass} rounded-pill">${cfg.level}</span>`);
       });
     });
 
