887b8ff73661134af9f38fe00182208bd39f2fbe
ascii-iie <96916359+ascii-iie@users.noreply.github.com>
2025-12-25 23:34:48 +0800

    论文标识更新

--- Page/js/replace_conf_tag_a.js
+++ Page/js/replace_conf_tag_a.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
     {
       level: "CCF-A",
       colorClass: "text-bg-danger",
-      confs: ["NeurIPS2025", "AAAI2025", "AAAI2023", "AAAI2022", "AAAI2021", "AAAI2020",
+      confs: ["AAAI2026", "NeurIPS2025", "AAAI2025", "AAAI2023", "AAAI2022", "AAAI2021", "AAAI2020",
               "ACL2025", "ACL2024", "ACL2023", "ACL2022", "ACL2021",
               "ICML2024", "KDD2025", "SIGIR2024", "SIGIR2022", "TKDE2023", "TOIS2025", "TOIS2023",
               "WWW2025", "WWW2024", "WWW2023", "WWW2022", "WWW2021", "WWW2020", "WWW2019"]
